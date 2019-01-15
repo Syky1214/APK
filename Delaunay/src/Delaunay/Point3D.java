ff06bacc8827ac31a3d8dc3bc45786eea89b8ef7
Matus <matussyky@gmail.com>
2019-01-15 15:15:09 +0100

    clear point3d and edge + coment

--- Delaunay/src/Delaunay/Point3D.java
+++ Delaunay/src/Delaunay/Point3D.java
@@ -11,35 +11,35 @@ import java.awt.geom.Point2D;
  *
  * @author Matus and Marek
  */
-
 // class, which representing data structure Point3D
 public class Point3D {
+
     //variables
     private double x;
     private double y;
     private double z;
-    
-    public Point3D(double ax, double ay, double az){
+
+    public Point3D(double ax, double ay, double az) {
         //initialized variables
         x = ax;
         y = ay;
         z = az;
     }
-    
-    public Point2D toPoint2D(){
-        return new Point2D.Double(x,y);
+
+    public Point2D toPoint2D() {
+        return new Point2D.Double(x, y);
     }
-    
+
     //getters and setters
-    public double getX(){
+    public double getX() {
         return x;
     }
-    
-    public double getY(){
+
+    public double getY() {
         return y;
     }
-    
-    public double getZ(){
+
+    public double getZ() {
         return z;
     }
 
@@ -54,5 +54,5 @@ public class Point3D {
     public void setZ(double z) {
         this.z = z;
     }
-    
+
 }
